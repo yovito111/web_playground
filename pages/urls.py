c4a322749cbb2833ec8ff501c444b3b5e9740a52
yovito <giovicr111@gmail.com>
2022-10-11 15:33:07 -0300

    Segundo commit del proyecto

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,7 @@ from . import views
 pages_patterns = ([
     path('', views.PagesListView.as_view(), name='pages'),
     path('<int:pk>/<slug:slug>/', views.PageDetailView.as_view(), name='page'),
-    path('created/', views.PageCreateView.as_view(), name='created')
+    path('created/', views.PageCreateView.as_view(), name='created'),
+    path('update/<int:pk>', views.PageUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', views.PageDeleteView.as_view(), name='delete')
 ], 'pages')
--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,12 @@
 """ Creando las vistas de PAges"""
 #Vistas basadas en clases
-from django.views.generic.edit import CreateView
+from ast import arg
+from django.views.generic.edit import CreateView ,UpdateView, DeleteView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.urls import reverse, reverse_lazy
+from django.urls import  reverse_lazy
 from .models import Page
-
+from .forms import PageForm
 
 class PagesListView(ListView):
     model = Page
@@ -15,5 +16,17 @@ class PageDetailView(DetailView):
 
 class PageCreateView(CreateView):
     model = Page
-    fields = ['title', 'content', 'order']
+    form_class = PageForm
+    success_url = reverse_lazy('pages:pages')
+
+class PageUpdateView(UpdateView):
+    model = Page
+    form_class = PageForm
+    template_name_suffix = '_update_form'
+    
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id]) +'?ok'
+
+class PageDeleteView(DeleteView):
+    model = Page
     success_url = reverse_lazy('pages:pages')
